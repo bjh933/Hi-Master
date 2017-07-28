b41943e1af0977399ed06bb8101ebda420061183
bjh <bjh2066@gmail.com>
2017-07-28 17:58:47 +0900

    facebook login

--- app/src/main/java/com/example/a1/himaster/MainActivity.java
+++ app/src/main/java/com/example/a1/himaster/MainActivity.java
@@ -1,31 +1,34 @@
 package com.example.a1.himaster;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.ImageView;
+import java.util.Arrays;
 
-import static android.view.LayoutInflater.from;
+import android.widget.Button;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
 
 public class MainActivity extends AppCompatActivity {
     Button skip;
-
-
+    Button CustomloginButton;
+    private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-
+        FacebookSdk.sdkInitialize(getApplicationContext()); // SDK 초기화 (setContentView 보다 먼저 실행되어야합니다. 안그럼 에러납니다.)
         setContentView(R.layout.activity_main);
-
+        callbackManager = CallbackManager.Factory.create();  //로그인 응답을 처리할 콜백 관리자
+        CustomloginButton = (Button)findViewById(R.id.facebookBtn);
         skip = (Button)findViewById(R.id.skipBtn);
 
         skip.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +41,38 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+
+        CustomloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //LoginManager - 요청된 읽기 또는 게시 권한으로 로그인 절차를 시작합니다.
+                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this,
+                        Arrays.asList("public_profile", "user_friends"));
+                LoginManager.getInstance().registerCallback(callbackManager,
+                        new FacebookCallback<LoginResult>() {
+                            @Override
+                            public void onSuccess(LoginResult loginResult) {
+                                Log.e("onSuccess", "onSuccess");
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                Log.e("onCancel", "onCancel");
+                            }
+
+                            @Override
+                            public void onError(FacebookException exception) {
+                                Log.e("onError", "onError " + exception.getLocalizedMessage());
+                            }
+                        });
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
